d8948f343a3ac19e9995222ae47ec17201854396
ysk <shaokai.ysk@alibaba-inc.com>
2018-10-30 19:42:09 +0800

    S=1+2+3+.....+n S=1+3+5+.....+(2n-1) S=2+4+6+......+2n

--- src/main/java/com/taobao/math/SumUtil.java
+++ src/main/java/com/taobao/math/SumUtil.java
@@ -1,5 +1,9 @@
 package com.taobao.math;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 计算合
  *
@@ -18,16 +22,12 @@ public class SumUtil {
      * @return
      */
     public static int sumOfN(int n) {
-        StringBuilder number = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if (number.length() == 0) {
-                number.append(i);
-            } else {
-                number.append("+" + i);
-            }
+            numbers.add(i);
         }
 
-        System.out.println("求和：" + number);
+        System.out.println("求和：" + format(numbers));
         return n * (n + 1) / 2;
     }
 
@@ -40,17 +40,13 @@ public class SumUtil {
      * @return
      */
     public static int sumOfOdd(int n) {
-        StringBuilder odd = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if (odd.length() == 0) {
-                odd.append((2 * i - 1));
-            } else {
-                odd.append("+" + (2 * i - 1));
-            }
+            numbers.add(2 * i - 1);
 
         }
 
-        System.out.println("求奇数和：" + odd);
+        System.out.println("求奇数和：" + format(numbers));
 
         return n * n;
     }
@@ -64,17 +60,17 @@ public class SumUtil {
      * @return
      */
     public static int sumOfEven(int n) {
-        StringBuilder even = new StringBuilder();
+        List<Integer> numbers = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            if (even.length() == 0) {
-                even.append(2 * i);
-            } else {
-                even.append("+" + 2 * i);
-            }
+            numbers.add(2 * i);
         }
 
-        System.out.println("求偶数和：" + even);
+        System.out.println("求偶数和：" + format(numbers));
         return n * (n + 1);
+    }
+
+    private static String format(List<Integer> numbers) {
+        return numbers.stream().map(i -> Integer.toString(i)).collect(Collectors.joining("+"));
     }
 
     public static void main(String[] args) {
